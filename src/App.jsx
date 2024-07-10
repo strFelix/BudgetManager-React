731d1e0b6fce28276881efc0e9cbd95ca6ec66af
Lucas Felix <Lucas@glide.com.br>
2024-07-10 14:49:50 -0300

    feat: add base components and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { useState } from "react";
+//styles
+import "./App.css";
+//components
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
 
 function App() {
   return (
     <>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contato</Link>
+            </li>
+            <li>
+              <Link to="/company">Empresa</Link>
+            </li>
+            <li>
+              <Link to="/newproject">Novo projeto</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/company" element={<Company />} />
+          <Route exact path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
