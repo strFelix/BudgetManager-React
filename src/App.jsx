1becf5fda621ce3e181e83a1ab048015b27158b1
Lucas Felix <Lucas@glide.com.br>
2024-07-10 15:27:45 -0300

    feat: add layout container component

--- src/App.jsx
+++ src/App.jsx
@@ -7,33 +7,26 @@ import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Company from "./components/pages/Company";
 import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
 
 function App() {
   return (
     <>
       <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contato</Link>
-            </li>
-            <li>
-              <Link to="/company">Empresa</Link>
-            </li>
-            <li>
-              <Link to="/newproject">Novo projeto</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/contact" element={<Contact />} />
-          <Route exact path="/company" element={<Company />} />
-          <Route exact path="/newproject" element={<NewProject />} />
-        </Routes>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contato</Link>
+          <Link to="/company">Empresa</Link>
+          <Link to="/newproject">Novo projeto</Link>
+        </div>
+        <Container customClass="min-height">
+          <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/contact" element={<Contact />} />
+              <Route exact path="/company" element={<Company />} />
+              <Route exact path="/newproject" element={<NewProject />} />
+          </Routes>
+        </Container>
       </Router>
     </>
   );
