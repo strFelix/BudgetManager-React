5b702205dedf338e70d0b0001a27146dcb5da8bc
Lucas Felix <Lucas@glide.com.br>
2024-07-12 09:53:10 -0300

    feat(form): add handdle functions

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import styles from './css/newProject.module.css'
-import ProjectForm from '../project/ProjectForm'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./css/newProject.module.css";
+import ProjectForm from "../project/ProjectForm";
 
 const NewProject = () => {
+  
+  const navigate = useNavigate();
+  function CreatePost(project) {
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      // redirect
+      .then(navigate("/projects"))
+      .catch((err) => console.error(err));
+  }
+
   return (
-    <div className={styles.newPeojectContainer }>
+    <div className={styles.newPeojectContainer}>
       <h1>Criar projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços!</p>
-      <ProjectForm btnText='Criar projeto'></ProjectForm>
+      <ProjectForm handleSubmit={CreatePost} btnText="Criar projeto"></ProjectForm>
     </div>
-  )
-}
+  );
+};
 
-export default NewProject
+export default NewProject;
--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,12 +1,12 @@
-
 import { useState, useEffect } from "react";
 import styles from "./css/projectForm.module.css";
 import Input from "../form/Input.jsx";
 import Select from "../form/Select.jsx";
 import SubmitButton from "../form/SubmitButton.jsx";
 
-const ProjectForm = ({ btnText }) => {
+const ProjectForm = ({ btnText, handleSubmit, projectData }) => {
   const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState(projectData || {});
 
   useEffect(() => {
     fetch("http://localhost:5000/categories", {
@@ -20,21 +20,50 @@ const ProjectForm = ({ btnText }) => {
       .catch((err) => console.error(err));
   }, []);
 
+  const submit = (e) => {
+    e.preventDefault();
+    handleSubmit(project);
+  };
+
+  const handleChange = (e) => {
+    setProject({ ...project, [e.target.name]: e.target.value });
+  };
+
+  const handleCategory = (e) => {
+    setProject({
+      ...project,
+      category: {
+        id: e.target.value,
+        name: e.target.options[e.target.selectedIndex].text,
+      },
+    });
+  };
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type="text"
         text="Nome do projeto"
         name="name"
         placeholder="Insira o nome do projeto"
+        handleOnChange={handleChange}
+        value={project.name ? project.name : ""}
       />
       <Input
         type="number"
         text="Orçamento do projeto"
         name="budget"
         placeholder="Insira o orçamento total do projeto"
+        handleOnChange={handleChange}
+        value={project.budget ? project.budget : ""}
       />
-      <Select name="category_id" text="Selecione a categoria" options={categories}/>
+      <Select
+        name="category_id"
+        text="Selecione a categoria"
+        options={categories}
+        handleOnChange={handleCategory}
+        value={project.category ? project.category.id : ""}
+      />
       <SubmitButton text={btnText} />
     </form>
   );
