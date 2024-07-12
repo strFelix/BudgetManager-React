1089b66f4215cecd73a306ce52392689dd61c0b5
Lucas Felix <Lucas@glide.com.br>
2024-07-12 10:51:09 -0300

    style: add projets page  starter layout

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,17 +1,25 @@
-import React from "react";
 import { useLocation } from "react-router-dom";
+import React from "react";
+import styles from "./css/projects.module.css";
 import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 
 const Projects = () => {
-  
   const location = useLocation();
   let message = location.state ? location.state.message : "";
   let type = location.state ? location.state.type : "";
 
   return (
-    <div>
-      <h1>Meus projetos</h1>
-      <Message type={type} msg={message}/>
+    <div className={styles.projectContainer}>
+      <div className={styles.titleContainer}>
+        <h1>Meus projetos</h1>
+        <LinkButton to="/newproject" text="Criar projeto"></LinkButton>
+      </div>
+      <Message type={type} msg={message} />
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   );
 };
